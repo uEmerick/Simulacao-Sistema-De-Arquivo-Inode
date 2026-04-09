3315d4391204dc7a0b86a9df687d15b040515efa
Gustavo Badan <gustavobadan01@gmail.com>
2026-04-08 23:45:05 -0300

    fix: Adicionar Permissao.h atualizado e remover split.h descontinuado

--- Permissao.h
+++ Permissao.h
@@ -1,84 +1,84 @@
-int permissaoToInt(char permissao[]){
-    int chmodCod = 0;
+int converterPermissaoParaInteiro(char permissao[]){
+    int codigoModificacao = 0;
     
     if (permissao[0] == 'r'){
-        chmodCod += 4;
+        codigoModificacao += 4;
     }
     
     if (permissao[1] == 'w'){
-        chmodCod += 2;
+        codigoModificacao += 2;
     }
     
     if (permissao[2] == 'x'){
-        chmodCod += 1;
+        codigoModificacao += 1;
     }
     
-    return chmodCod;
+    return codigoModificacao;
 }
 
-void convertPermissaoToString(int permissao, char retorno[], int posicaoInicial=0)
+void converterPermissaoParaTexto(int permissao, char retorno[], int indiceInicial=0)
 {
     if(permissao - 4 >= 0)
 	{
-        retorno[posicaoInicial] = 'r';
+        retorno[indiceInicial] = 'r';
         permissao -= 4;
     }
 	else
-        retorno[posicaoInicial] = '-';
+        retorno[indiceInicial] = '-';
     
     if(permissao - 2 >= 0)
 	{
-        retorno[posicaoInicial+1] = 'w';
+        retorno[indiceInicial+1] = 'w';
         permissao -= 2;
     }
 	else
-        retorno[posicaoInicial+1] = '-';
+        retorno[indiceInicial+1] = '-';
     
     if(permissao - 1 >= 0)
 	{
-        retorno[posicaoInicial+2] = 'x';
+        retorno[indiceInicial+2] = 'x';
         permissao -= 1;
     }
 	else
-        retorno[posicaoInicial+2] = '-';
+        retorno[indiceInicial+2] = '-';
 
-    retorno[posicaoInicial+3] = '\0';
+    retorno[indiceInicial+3] = '\0';
 }
 
-void convertPermissaoUGOToString(int permissaoCompleta, char retorno[], int posicaoInicial=0)
+void converterPermissaoUGOParaTexto(int permissaoCompleta, char retorno[], int indiceInicial=0)
 {
-    char permissaoCompletaChar[4];
-    itoa(permissaoCompleta, permissaoCompletaChar, 10);
+    char arrayPermissaoCompleta[4];
+    itoa(permissaoCompleta, arrayPermissaoCompleta, 10);
 
-    int perm[3] = {
-        permissaoCompletaChar[0] - '0',
-        permissaoCompletaChar[1] - '0',
-        permissaoCompletaChar[2] - '0'
+    int permissao[3] = {
+        arrayPermissaoCompleta[0] - '0',
+        arrayPermissaoCompleta[1] - '0',
+        arrayPermissaoCompleta[2] - '0'
     };
 
-    convertPermissaoToString(perm[0], retorno, posicaoInicial);
-    convertPermissaoToString(perm[1], retorno, posicaoInicial+3);
-    convertPermissaoToString(perm[2], retorno, posicaoInicial+6);
+    converterPermissaoParaTexto(permissao[0], retorno, indiceInicial);
+    converterPermissaoParaTexto(permissao[1], retorno, indiceInicial+3);
+    converterPermissaoParaTexto(permissao[2], retorno, indiceInicial+6);
 
-    retorno[posicaoInicial+9] = '\0';
+    retorno[indiceInicial+9] = '\0';
 }
 
-int convertPermissaoUGOToInt(char permissaoCompleta[], int posicaoInicial=0)
+int converterPermissaoUGOParaInteiro(char permissaoCompleta[], int indiceInicial=0)
 {
     int permissao = 0;
-    char permissaoIndividual[3];
+    char arrayPermissaoIndividual[3];
 
     //usuario (7xx)
-    strncpy(permissaoIndividual, permissaoCompleta+posicaoInicial, 3);
-    permissao += permissaoToInt(permissaoIndividual) * 100;
+    strncpy(arrayPermissaoIndividual, permissaoCompleta+indiceInicial, 3);
+    permissao += converterPermissaoParaInteiro(arrayPermissaoIndividual) * 100;
 
     //grupo (x7x)
-    strncpy(permissaoIndividual, permissaoCompleta+posicaoInicial+3, 3);
-    permissao += permissaoToInt(permissaoIndividual) * 10;
+    strncpy(arrayPermissaoIndividual, permissaoCompleta+indiceInicial+3, 3);
+    permissao += converterPermissaoParaInteiro(arrayPermissaoIndividual) * 10;
 
     //outros (xx7)
-    strncpy(permissaoIndividual, permissaoCompleta+posicaoInicial+6, 3);
-    permissao += permissaoToInt(permissaoIndividual);
+    strncpy(arrayPermissaoIndividual, permissaoCompleta+indiceInicial+6, 3);
+    permissao += converterPermissaoParaInteiro(arrayPermissaoIndividual);
 
     return permissao;
 }
